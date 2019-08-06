2c49f7a1bd86ab75eb26f9991c950064ffdef88f
jiashun <jiashun@outlook.com>
2019-08-07 01:11:15 +0800

    修改临时车流水展示

--- src/main/resources/static/js/appjs/sys/finance/finance.js
+++ src/main/resources/static/js/appjs/sys/finance/finance.js
@@ -67,7 +67,7 @@ function load() {
 										} else if (value == '2') {
 											return '<span class="label label-primary">支付宝支付</span>';
 										} else if (value == '3') {
-											return '<span class="label label-primary">无需支付</span>';
+											return '<span class="label label-blue">无需支付</span>';
 										}
 									}
 								},
