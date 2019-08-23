264a248d7fe1f68c1200a5c180a260ef11aa98ad
luliang <1083825415@qq.com>
2019-08-23 16:48:23 +0800

    修改临时车流水展示-缴费类型修改，增加月租车

--- src/test/java/com/nuoze/cctower/service/TestTcpMemberService.java
+++ src/test/java/com/nuoze/cctower/service/TestTcpMemberService.java
@@ -4,6 +4,7 @@ import com.nuoze.cctower.dao.MemberDAO;
 import com.nuoze.cctower.pojo.entity.Member;
 import com.nuoze.cctower.pojo.vo.MemberVO;
 import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,18 +27,14 @@ public class TestTcpMemberService {
 
     @Test
     public void testMemberDao() {
-        List<Member> list = memberDAO.list(null);
-        for (Member member : list) {
-            System.out.println(member);
-        }
+        Assert.assertNotNull(memberDAO.list(null)) ;
     }
     @Test
     public void testList() {
         Map<String, Object> map = new HashMap<>();
         map.put("offset", 0) ;
         map.put("limit", 10) ;
-        Pair<Integer, List<MemberVO>> list = tcpMemberService.list(map);
-        System.out.println("*******" + list);
+        Assert.assertNotNull(tcpMemberService.list(map)) ;
     }
 
 }
