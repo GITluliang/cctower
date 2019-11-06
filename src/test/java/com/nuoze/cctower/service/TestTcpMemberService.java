dfe1a95ef4935470b3551b81701f1ce504b5c33d
luliang <1083825415@qq.com>
2019-11-06 22:28:50 +0800

    合并代码

--- src/test/java/com/nuoze/cctower/service/TestTcpMemberService.java
+++ src/test/java/com/nuoze/cctower/service/TestTcpMemberService.java
@@ -1,43 +1,11 @@
 package com.nuoze.cctower.service;
 
-import com.nuoze.cctower.component.BillingComponent;
-import com.nuoze.cctower.component.PaymentComponent;
-import com.nuoze.cctower.dao.OrderNumberDAO;
-
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.math.BigDecimal;
-import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestTcpMemberService {
 
-    @Autowired
-    private OrderNumberDAO orderNumberDAO ;
-    @Autowired
-    private PaymentComponent paymentComponent;
-
-    @Test
-    public void test() {
-/*        OrderNumber orderNumber = new OrderNumber();
-        orderNumber.setCreateTime(new Date());
-        orderNumber.setOrderSn(UUID.randomUUID().toString());
-        orderNumber.setParkingRecordId(1L);
-        orderNumberDAO.insert(orderNumber) ;*/
-        Assert.assertNotNull(orderNumberDAO.findByorderSn("ba0ce293-c3ba-429b-9db1-f00d2164978a"));
-    }
-
-    @Test
-    public void money() {
-        BigDecimal bigDecimal = new BigDecimal(10);
-        BigDecimal serviceCharge = paymentComponent.getServiceCharge(bigDecimal, 5);
-        System.out.println(serviceCharge);
-        System.out.println(bigDecimal.subtract(serviceCharge));
-    }
 }
