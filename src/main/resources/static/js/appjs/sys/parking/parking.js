271c29fbab286c5cd8556174b83dbac941fceab7
luliang <luliang@git.com>
2019-11-29 00:34:15 +0800

    Merge branch 'develop' into luliang
    # Conflicts:
    #	src/test/java/com/nuoze/cctower/service/TestTcpMemberService.java

--- src/main/resources/static/js/appjs/sys/parking/parking.js
+++ src/main/resources/static/js/appjs/sys/parking/parking.js
@@ -9,9 +9,9 @@ function load() {
             {
                 method: 'get', // 服务器数据的请求方式 get or post
                 url: prefix + "/list", // 服务器数据的加载地址
-                //	showRefresh : true,
-                //	showToggle : true,
-                //	showColumns : true,
+                showRefresh : true,
+                showToggle : true,
+                showColumns : true,
                 iconSize: 'outline',
                 toolbar: '#exampleToolbar',
                 striped: true, // 设置为true会有隔行变色效果
@@ -93,7 +93,10 @@ function load() {
                                 + '\')"><i class="fa fa-key"></i></a> ';
                                 return e + d;
                         }
-                    }]
+                    }],
+                //********************
+                showExport: true,
+                exportTypes: ['excel'],
             });
 }
 
